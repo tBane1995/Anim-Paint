4c3b37276e9ff34854df5cacb4a8fba66a361d2f
unknown <bboguslawski1995@gmail.com>
2025-09-21 01:39:35 +0200

    extract Slider.hpp into .hpp and .cpp files

--- Anim-Paint/include/Slider.hpp
+++ Anim-Paint/include/Slider.hpp
@@ -1,5 +1,6 @@
-﻿#ifndef Slider_hpp
-#define Slider_hpp
+﻿#pragma once
+#include "ElementGUI.hpp"
+#include "Textures.hpp"
 
 enum class SliderState { Idle, Changed };
 
@@ -18,94 +19,16 @@ public:
 	SliderState state;
 	sf::Vector2f offset;
 
-	Slider(int min_value, int max_value) : ElementGUI() {
-		this->min_value = min_value;
-		this->max_value = max_value;
-		current_value = min_value;
+	Slider(int min_value, int max_value);
+	~Slider();
 
-		bar_texture = getTexture(L"tex\\slider\\bar.png");
-		bar_sprite = sf::Sprite(*bar_texture->texture);
-		bar_sprite.setScale(128 / 8, 1.0f);
+	void setValue(int value);
+	int getValue();
+	sf::Vector2f getSliderPosition();
+	void setPosition(sf::Vector2f position);
 
-		slider_texture = getTexture(L"tex\\slider\\slider.png");
-		slider_sprite = sf::Sprite(*slider_texture->texture);
-
-		state = SliderState::Idle;
-	}
-
-	~Slider() { }
-
-	void setValue(int value) {
-		current_value = value;
-
-		if (current_value < min_value)
-			current_value = min_value;
-		else if (current_value > max_value)
-			current_value = max_value;
-
-		slider_sprite.setPosition(getSliderPosition());
-
-	}
-
-	int getValue() {
-		return current_value;
-	}
-
-	sf::Vector2f getSliderPosition() {
-		float line_length = bar_sprite.getGlobalBounds().getSize().x - 16;
-
-		sf::Vector2f result;
-		result.x = bar_sprite.getPosition().x + float(current_value - min_value) * line_length / float(max_value - min_value);
-		result.y = bar_sprite.getPosition().y - 4;
-
-		return result;
-	}
-
-	void setPosition(sf::Vector2f position) {
-		bar_sprite.setPosition(position);
-		slider_sprite.setPosition(getSliderPosition());
-	}
-
-	void cursorHover() { 
-		
-	}
-	void handleEvent(sf::Event& event) {
-
-		if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left) {
-			if (slider_sprite.getGlobalBounds().contains(worldMousePosition)) {
-				ElementGUI_pressed = this;
-				offset = worldMousePosition - slider_sprite.getPosition();
-				state = SliderState::Changed;
-
-			}
-			
-		}
-		else if (event.type == sf::Event::MouseButtonReleased && event.mouseButton.button == sf::Mouse::Left) {
-			if (ElementGUI_pressed == this) {
-				ElementGUI_pressed = nullptr;
-				state = SliderState::Idle;
-			}
-		}
-
-	}
-
-	void update() {
-		if (state == SliderState::Changed) {
-			sf::Vector2f newPos = worldMousePosition - offset;
-			newPos.y = bar_sprite.getPosition().y - 4;
-			newPos.x = std::clamp(newPos.x, bar_sprite.getPosition().x, bar_sprite.getPosition().x + bar_sprite.getGlobalBounds().getSize().x - 16);
-			slider_sprite.setPosition(newPos);
-
-			current_value = float(slider_sprite.getPosition().x - bar_sprite.getPosition().x)/ (bar_sprite.getGlobalBounds().getSize().x-16) * (float(max_value - min_value)) + min_value;
-		}
-	}
-
-	
-	
-	void draw() {
-		window->draw(bar_sprite);
-		window->draw(slider_sprite);
-	}
+	void cursorHover();
+	void handleEvent(sf::Event& event);
+	void update();
+	void draw();
 };
-
-#endif
---
+++ Anim-Paint/src/Slider.cpp
@@ -0,0 +1,97 @@
+﻿#include "Slider.hpp"
+#include "ElementGUI.hpp"
+#include "Textures.hpp"
+#include "SFML/Graphics.hpp"
+#include "Mouse.hpp"
+#include "Window.hpp"
+
+Slider::Slider(int min_value, int max_value) : ElementGUI() {
+	this->min_value = min_value;
+	this->max_value = max_value;
+	current_value = min_value;
+
+	bar_texture = getTexture(L"tex\\slider\\bar.png");
+	bar_sprite = sf::Sprite(*bar_texture->texture);
+	bar_sprite.setScale(128 / 8, 1.0f);
+
+	slider_texture = getTexture(L"tex\\slider\\slider.png");
+	slider_sprite = sf::Sprite(*slider_texture->texture);
+
+	state = SliderState::Idle;
+}
+
+Slider::~Slider() {
+
+}
+
+void Slider::setValue(int value) {
+	current_value = value;
+
+	if (current_value < min_value)
+		current_value = min_value;
+	else if (current_value > max_value)
+		current_value = max_value;
+
+	slider_sprite.setPosition(getSliderPosition());
+
+}
+
+int Slider::getValue() {
+	return current_value;
+}
+
+sf::Vector2f Slider::getSliderPosition() {
+	float line_length = bar_sprite.getGlobalBounds().getSize().x - 16;
+
+	sf::Vector2f result;
+	result.x = bar_sprite.getPosition().x + float(current_value - min_value) * line_length / float(max_value - min_value);
+	result.y = bar_sprite.getPosition().y - 4;
+
+	return result;
+}
+
+void Slider::setPosition(sf::Vector2f position) {
+	bar_sprite.setPosition(position);
+	slider_sprite.setPosition(getSliderPosition());
+}
+
+void Slider::cursorHover() {
+
+}
+void Slider::handleEvent(sf::Event& event) {
+
+	if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left) {
+		if (slider_sprite.getGlobalBounds().contains(worldMousePosition)) {
+			ElementGUI_pressed = this;
+			offset = worldMousePosition - slider_sprite.getPosition();
+			state = SliderState::Changed;
+
+		}
+
+	}
+	else if (event.type == sf::Event::MouseButtonReleased && event.mouseButton.button == sf::Mouse::Left) {
+		if (ElementGUI_pressed == this) {
+			ElementGUI_pressed = nullptr;
+			state = SliderState::Idle;
+		}
+	}
+
+}
+
+void Slider::update() {
+	if (state == SliderState::Changed) {
+		sf::Vector2f newPos = worldMousePosition - offset;
+		newPos.y = bar_sprite.getPosition().y - 4;
+		newPos.x = std::clamp(newPos.x, bar_sprite.getPosition().x, bar_sprite.getPosition().x + bar_sprite.getGlobalBounds().getSize().x - 16);
+		slider_sprite.setPosition(newPos);
+
+		current_value = float(slider_sprite.getPosition().x - bar_sprite.getPosition().x) / (bar_sprite.getGlobalBounds().getSize().x - 16) * (float(max_value - min_value)) + min_value;
+	}
+}
+
+
+
+void Slider::draw() {
+	window->draw(bar_sprite);
+	window->draw(slider_sprite);
+}
