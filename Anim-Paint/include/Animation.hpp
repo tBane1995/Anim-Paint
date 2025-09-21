33b53846ffe287e8fba2eab97395787e1adf896e
unknown <bboguslawski1995@gmail.com>
2025-09-21 17:20:16 +0200

    extract Animation.hpp into .hpp and .cpp files

--- Anim-Paint/include/Animation.hpp
+++ Anim-Paint/include/Animation.hpp
@@ -1,5 +1,5 @@
-﻿#ifndef Animation_hpp
-#define Animation_hpp
+﻿#pragma once
+#include "Frame.hpp"
 
 class Animation {
 public:
@@ -7,145 +7,26 @@ public:
 	int currentFrame;
 	int currentLayer;
 
-	Animation() {
+	Animation();
+	~Animation();
 
-		currentFrame = 0;
-		currentLayer = 0;
-
-		int size = 64;
-
-		Frame* frame;
-		
-		{
-			frame = new Frame();
-			std::vector < Layer* >& layers = frame->getLayers();
-			layers.push_back(new Layer(L"Layer " + std::to_wstring(layers.size()), sf::Vector2i(size, size)));
-			layers.push_back(new Layer(L"Layer " + std::to_wstring(layers.size()), sf::Vector2i(size, size)));
-			layers.push_back(new Layer(L"Layer " + std::to_wstring(layers.size()), sf::Vector2i(size, size)));
-			layers.push_back(new Layer(L"Layer " + std::to_wstring(layers.size()), sf::Vector2i(size, size)));
-			frames.push_back(frame);
-		}
-
-		{
-			frame = new Frame();
-			std::vector < Layer* >& layers = frame->getLayers();
-			layers.push_back(new Layer(L"Layer " + std::to_wstring(layers.size()), sf::Vector2i(size, size)));
-			layers.push_back(new Layer(L"Layer " + std::to_wstring(layers.size()), sf::Vector2i(size, size)));
-			layers.push_back(new Layer(L"Layer " + std::to_wstring(layers.size()), sf::Vector2i(size, size)));
-			layers.push_back(new Layer(L"Layer " + std::to_wstring(layers.size()), sf::Vector2i(size, size)));
-			frames.push_back(frame);
-		}
-
-		lastLayer();
-		
-	}
-
-	~Animation() {
-
-	}
-
-	Frame* getCurrentFrame() {
-		// return current Frame
-		if (frames.empty())
-			return nullptr;
-
-		return frames[currentFrame];
-	}
-
-	std::vector < Layer* > getLayers() {
-		// return current Layers
-		if (frames.empty())
-			return std::vector < Layer* >();
-
-		return getCurrentFrame()->getLayers();
-	}
-
-	Layer* getCurrentLayer() {
-		// return current Layer
-		if (frames.empty())
-			return nullptr;
-
-		return getCurrentFrame()->getLayers()[currentLayer];
-	}
-
-	std::vector < Frame* > getFrames() {
-		return frames;
-	}
-
-	Layer* getLayer(int id) {
-		// return Layer
-		if (id < 0) 
-			return nullptr;
-
-		else if (id >= getCurrentFrame()->getLayers().size())
-			return nullptr;
-
-		else 
-			return getCurrentFrame()->getLayers()[id];
-	}
-
-	int getCurrentFrameID() {
-		// return current Frame ID
-		return currentFrame;
-	}
-
-	int getCurrentLayerID() {
-		// return current Layer ID
-		return currentLayer;
-	}
-
-	int getFramesSize() {
-		// return Frames size
-		return frames.size();
-	}
-
-	int getLayersSize() {
-		// return current Layers size
-		return getCurrentFrame()->layers.size();
-	}
-
-	void setCurrentFrameID(int id) {
-		// set current Frame ID
-		currentFrame = id;
-	}
-
-	void setCurrentLayerID(int id) {
-		// set current Layer ID
-		currentLayer = id;
-	}
-
-	void firstFrame() {
-		currentFrame = 0;
-	}
-
-	void lastFrame() {
-		currentFrame = frames.size() - 1;
-	}
-
-	void nextFrame() {
-		currentFrame += 1;
-
-		if (currentFrame >= frames.size())
-			currentFrame = 0;
-	}
-
-	void prevFrame() {
-		currentFrame -= 1;
-
-		if (currentFrame < 0)
-			currentFrame = frames.size() - 1;
-	}
-
-	void firstLayer() {
-		currentLayer = 0;
-	}
-
-	void lastLayer() {
-		currentLayer = getCurrentFrame()->layers.size() - 1;
-	}
+	Frame* getCurrentFrame();
+	std::vector < Layer* > getLayers();
+	Layer* getCurrentLayer();
+	std::vector < Frame* > getFrames();
+	Layer* getLayer(int id);
+	int getCurrentFrameID();
+	int getCurrentLayerID();
+	int getFramesSize();
+	int getLayersSize();
+	void setCurrentFrameID(int id);
+	void setCurrentLayerID(int id);
+	void firstFrame();
+	void lastFrame();
+	void nextFrame();
+	void prevFrame();
+	void firstLayer();
+	void lastLayer();
 };
 
-
-Animation* animation = nullptr;
-
-#endif
+extern Animation* animation;
---
+++ Anim-Paint/src/Animation.cpp
@@ -0,0 +1,140 @@
+﻿#include "Animation.hpp"
+
+Animation::Animation() {
+
+	currentFrame = 0;
+	currentLayer = 0;
+
+	int size = 64;
+
+	Frame* frame;
+
+	{
+		frame = new Frame();
+		std::vector < Layer* >& layers = frame->getLayers();
+		layers.push_back(new Layer(L"Layer " + std::to_wstring(layers.size()), sf::Vector2i(size, size)));
+		layers.push_back(new Layer(L"Layer " + std::to_wstring(layers.size()), sf::Vector2i(size, size)));
+		layers.push_back(new Layer(L"Layer " + std::to_wstring(layers.size()), sf::Vector2i(size, size)));
+		layers.push_back(new Layer(L"Layer " + std::to_wstring(layers.size()), sf::Vector2i(size, size)));
+		frames.push_back(frame);
+	}
+
+	{
+		frame = new Frame();
+		std::vector < Layer* >& layers = frame->getLayers();
+		layers.push_back(new Layer(L"Layer " + std::to_wstring(layers.size()), sf::Vector2i(size, size)));
+		layers.push_back(new Layer(L"Layer " + std::to_wstring(layers.size()), sf::Vector2i(size, size)));
+		layers.push_back(new Layer(L"Layer " + std::to_wstring(layers.size()), sf::Vector2i(size, size)));
+		layers.push_back(new Layer(L"Layer " + std::to_wstring(layers.size()), sf::Vector2i(size, size)));
+		frames.push_back(frame);
+	}
+
+	lastLayer();
+
+}
+
+Animation::~Animation() {
+
+}
+
+Frame* Animation::getCurrentFrame() {
+	// return current Frame
+	if (frames.empty())
+		return nullptr;
+
+	return frames[currentFrame];
+}
+
+std::vector < Layer* > Animation::getLayers() {
+	// return current Layers
+	if (frames.empty())
+		return std::vector < Layer* >();
+
+	return getCurrentFrame()->getLayers();
+}
+
+Layer* Animation::getCurrentLayer() {
+	// return current Layer
+	if (frames.empty())
+		return nullptr;
+
+	return getCurrentFrame()->getLayers()[currentLayer];
+}
+
+std::vector < Frame* > Animation::getFrames() {
+	return frames;
+}
+
+Layer* Animation::getLayer(int id) {
+	// return Layer
+	if (id < 0)
+		return nullptr;
+
+	else if (id >= getCurrentFrame()->getLayers().size())
+		return nullptr;
+
+	else
+		return getCurrentFrame()->getLayers()[id];
+}
+
+int Animation::getCurrentFrameID() {
+	// return current Frame ID
+	return currentFrame;
+}
+
+int Animation::getCurrentLayerID() {
+	// return current Layer ID
+	return currentLayer;
+}
+
+int Animation::getFramesSize() {
+	// return Frames size
+	return frames.size();
+}
+
+int Animation::getLayersSize() {
+	// return current Layers size
+	return getCurrentFrame()->layers.size();
+}
+
+void Animation::setCurrentFrameID(int id) {
+	// set current Frame ID
+	currentFrame = id;
+}
+
+void Animation::setCurrentLayerID(int id) {
+	// set current Layer ID
+	currentLayer = id;
+}
+
+void Animation::firstFrame() {
+	currentFrame = 0;
+}
+
+void Animation::lastFrame() {
+	currentFrame = frames.size() - 1;
+}
+
+void Animation::nextFrame() {
+	currentFrame += 1;
+
+	if (currentFrame >= frames.size())
+		currentFrame = 0;
+}
+
+void Animation::prevFrame() {
+	currentFrame -= 1;
+
+	if (currentFrame < 0)
+		currentFrame = frames.size() - 1;
+}
+
+void Animation::firstLayer() {
+	currentLayer = 0;
+}
+
+void Animation::lastLayer() {
+	currentLayer = getCurrentFrame()->layers.size() - 1;
+}
+
+Animation* animation = nullptr;
