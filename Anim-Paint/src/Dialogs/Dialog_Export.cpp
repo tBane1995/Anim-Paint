879fda24196422d8567aa6b04e4a263997e65045
unknown <bboguslawski1995@gmail.com>
2026-03-18 12:11:30 +0100

    Refactor - Export Using Confirm Dialog

--- Anim-Paint/src/Dialogs/Dialog_Export.cpp
+++ Anim-Paint/src/Dialogs/Dialog_Export.cpp
@@ -5,15 +5,27 @@
 
 Dialog_Export::Dialog_Export() : FileDialog(L"Export As File", L"export", L".png") {
 
-	this->_selectBtn->_onclick_func = [this]() {
+	_selectBtn->_onclick_func = [this]() {
 		std::wstring name = _filenameInput->getText();
 		std::filesystem::path fullPath = std::filesystem::path(currentPath) / name;
+
 		if (!(fullPath.extension() == ".png"))
 			fullPath += L".png";
-		//std::wcout << L"Export: " << fullPath << L"\n";
-		main_menu->exportAsFile(fullPath);
-		_state = DialogState::ToClose;
-		};
+
+		if (std::filesystem::exists(fullPath)) {
+			std::shared_ptr<ConfirmDialog> confirm = std::make_shared<ConfirmDialog>(L"File Already Exists", L"Do you want to replace it?");
+			confirm->_confirmBtn->_onclick_func = [this, fullPath, confirm]() {
+				main_menu->exportAsFile(fullPath);
+				_state = DialogState::ToClose;
+				confirm->_state = DialogState::ToClose;
+				};
+			confirm->_cancelBtn->_onclick_func = [this, confirm]() {
+				confirm->_state = DialogState::ToClose;
+				};
+			dialogs.push_back(confirm);
+		}
+	};
+
 
 	_onTabElements.push_back(_filenameInput);
 	_onTabElements.push_back(_selectBtn);
