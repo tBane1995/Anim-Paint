231300dfc1dac929bbf5e436245b1706938809c6
unknown <bboguslawski1995@gmail.com>
2026-03-18 12:17:02 +0100

    Refactor - Save Project Using Confirm Dialog

--- Anim-Paint/src/Dialogs/Dialog_Save_Project.cpp
+++ Anim-Paint/src/Dialogs/Dialog_Save_Project.cpp
@@ -1,15 +1,28 @@
 #include "Dialogs/Dialog_Save_Project.hpp"
 #include "Components/MainMenu/MainMenu.hpp"
-#include <iostream>
+#include "Dialogs/ConfirmDialog.hpp"
 
 Dialog_Save_Project::Dialog_Save_Project() : FileDialog(L"Save Project", L"save", L".animproj") {
 
-	this->_selectBtn->_onclick_func = [this]() {
+	_selectBtn->_onclick_func = [this]() {
 		std::wstring name = _filenameInput->getText();
 		std::filesystem::path fullPath = std::filesystem::path(currentPath) / name;
-		//std::wcout << L"Save: " << fullPath << L"\n";
-		main_menu->saveProject(fullPath);
-		_state = DialogState::ToClose;
+
+		if (!(fullPath.extension() == ".animproj"))
+			fullPath += L".animproj";
+
+		if (std::filesystem::exists(fullPath)) {
+			std::shared_ptr<ConfirmDialog> confirm = std::make_shared<ConfirmDialog>(L"File Already Exists", L"Do you want to replace it?");
+			confirm->_confirmBtn->_onclick_func = [this, fullPath, confirm]() {
+				main_menu->saveProject(fullPath);
+				_state = DialogState::ToClose;
+				confirm->_state = DialogState::ToClose;
+				};
+			confirm->_cancelBtn->_onclick_func = [this, confirm]() {
+				confirm->_state = DialogState::ToClose;
+				};
+			dialogs.push_back(confirm);
+		}
 		};
 
 	_onTabElements.push_back(_filenameInput);
