ef08b3f346db309bdcc3b0ca10f80c684d7c97c6
unknown <bboguslawski1995@gmail.com>
2026-03-29 23:09:06 +0200

    Add - Option With Icon

---
+++ Anim-Paint/include/Controls/OptionWithIcon.hpp
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include "Controls/Option.hpp"
+
+class OptionWithIcon : public Option {
+public:
+	std::shared_ptr<Texture> _icon;
+	std::shared_ptr<Texture> _icon_hover;
+
+	OptionWithIcon(std::wstring text, std::shared_ptr<Texture> texture, std::shared_ptr<Texture> hoverTexture, std::wstring shortcut = L"", sf::Vector2i position = sf::Vector2i(0, 0));
+	~OptionWithIcon();
+
+	void draw();
+};
---
+++ Anim-Paint/src/Controls/OptionWithIcon.cpp
@@ -0,0 +1,39 @@
+﻿#include "Controls/OptionWithIcon.hpp"
+#include "Theme.hpp"
+#include "Time.hpp"
+#include "Cursor.hpp"
+#include "Window.hpp"
+
+
+OptionWithIcon::OptionWithIcon(std::wstring text, std::shared_ptr<Texture> texture, std::shared_ptr<Texture> hoverTexture, std::wstring shortcut, sf::Vector2i position) : Option(text, shortcut, position) {
+	_icon = texture;
+	_icon_hover = hoverTexture;
+}
+
+OptionWithIcon::~OptionWithIcon() {
+
+}
+
+void OptionWithIcon::draw() {
+	Option::draw();
+	std::shared_ptr<Texture> texture;
+
+	switch (_state) {
+		case ButtonState::Idle:
+			texture = _icon;
+			break;
+		case ButtonState::Hover:
+			texture = _icon_hover;
+			break;
+		case ButtonState::Pressed:
+			texture = _icon_hover;
+			break;
+		default:
+			texture = _icon;
+			break;
+	};
+
+	sf::Sprite sprite(*texture->_texture);
+	sprite.setPosition(sf::Vector2f(_rect.position));
+	window->draw(sprite);
+}
