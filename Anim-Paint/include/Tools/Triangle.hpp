0e163a8cb0df0fb749411a4cf04a43b3a1eb591d
unknown <bboguslawski1995@gmail.com>
2026-04-04 15:30:59 +0200

    Add - Triangle Tool

---
+++ Anim-Paint/include/Tools/Triangle.hpp
@@ -0,0 +1,15 @@
+﻿#pragma once
+#include "SFML/Graphics.hpp"
+#include "Tools/ResizableTool.hpp"
+
+
+
+class Triangle : public ResizableTool {
+public:
+	Triangle();
+	~Triangle();
+
+	virtual void generateImage();
+};
+
+extern std::shared_ptr<ResizableTool> resizableTool;
