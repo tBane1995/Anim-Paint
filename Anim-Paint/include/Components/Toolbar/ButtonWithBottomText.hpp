458366d7c597c08957d3a387defe826d111a1648
unknown <bboguslawski1995@gmail.com>
2026-04-17 19:45:13 +0200

    Add - New Icons For Select And Increase/Decrease Tools

--- Anim-Paint/include/Components/Toolbar/ButtonWithBottomText.hpp
+++ Anim-Paint/include/Components/Toolbar/ButtonWithBottomText.hpp
@@ -8,9 +8,10 @@ public:
 	sf::Color _hoverTextColor;
 	std::shared_ptr<Texture> _texture;
 	std::shared_ptr<Texture> _hoverTexture;
+	std::shared_ptr<Texture> _pressTexture;
 	std::unique_ptr<sf::Text> _text;
 
-	ButtonWithBottomText(std::wstring text, sf::Color rectColor, sf::Color textColor, sf::Color hoverTextColor, std::shared_ptr<Texture> texture, std::shared_ptr<Texture> hoverTexture, sf::Vector2i position = sf::Vector2i(0, 0));
+	ButtonWithBottomText(std::wstring text, sf::Color rectColor, sf::Color textColor, sf::Color hoverTextColor, std::shared_ptr<Texture> texture, std::shared_ptr<Texture> hoverTexture, std::shared_ptr<Texture> pressTexture, sf::Vector2i position = sf::Vector2i(0, 0));
 	~ButtonWithBottomText();
 
 	void setPosition(sf::Vector2i position);
