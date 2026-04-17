0abca4302287c57b92944132b432b98e1306dd07
unknown <bboguslawski1995@gmail.com>
2026-04-17 20:10:18 +0200

    Refactor - Resizable Tool Clamping To Canvases

--- Anim-Paint/include/WorldToTileConverter.hpp
+++ Anim-Paint/include/WorldToTileConverter.hpp
@@ -4,5 +4,6 @@
 sf::Vector2i worldToTile(sf::Vector2i p, sf::Vector2i canvasPosition, sf::Vector2i size, float zoom, float zoom_delta);
 sf::Vector2i selectionToTile(sf::Vector2i p, sf::Vector2i canvasPosition, sf::Vector2i canvas_size, sf::Vector2i selection_size, sf::Vector2i selection_offset, float zoom, float zoom_delta);
 sf::Vector2i worldToTile(sf::Vector2i p, sf::Vector2i canvasPosition, float zoom, float zoom_delta);
+sf::Vector2i worldToTile(sf::Vector2i p, int canvasXPosition, int canvasYPosition, float zoom, float zoom_delta);
 sf::Vector2i tileToWorld(sf::Vector2i p, sf::Vector2i canvas_position, float zoom, float zoom_delta);
 int wrap(int v, int size);
